93e3146bc57e0b1eaa40dda33fe9c9d81be505e2
Marc-Oliver Teschke <marc@Marc-Oliver-Teschkes-MacBook.local>
2010-11-22 18:22:22 +0100

    Initial commit.

---
+++ Classes/_0daylist_iphoneAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  _0daylist_iphoneAppDelegate.h
+//  30daylist_iphone
+//
+//  Created by Marc-Oliver Teschke on 22.11.10.
+//  Copyright 2010 n/a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface _0daylist_iphoneAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
